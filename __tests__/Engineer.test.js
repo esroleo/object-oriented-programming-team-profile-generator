07d6467632f99635800eb20361e2497ee009a614
Esteban Romero <esroleo@gmail.com>
2021-02-13 13:27:25 -0500

    engineer class and jest test completed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,9 +1,46 @@
-const TestName = require('../lib/EngineerTest.js');
+const Engineer = require('../lib/Engineer.js');
 
 
-test('creates a employee object', () => {
-    const TestName = new TestName('Dave');
-    expect(employee.name).toBe('Dave');
+test('creates a manager object', () => {
+    const obj = {
+        name: "esteban",
+        id: 1234, 
+        email: "esroleo@gmail.com"
+    }
+
+    const gitHub = "esroleo"
+    const engineer = new Engineer(obj, gitHub);
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.gitHub).toEqual(expect.any(String));
 });
 
 
+test("gets github's engineer url path", () => {
+    const obj = {
+        name: "esteban",
+        id: 1234, 
+        email: "esroleo@gmail.com"
+    }
+
+    const gitHub = "esroleo"
+    const engineer = new Engineer(obj, gitHub);
+    expect(engineer.getGitHub()).toEqual(expect.any(String));
+    console.log(engineer.getGitHub());
+});
+
+
+
+  test("gets engineer's role", () => {
+    const obj = {
+        name: "esteban",
+        id: 1234, 
+        email: "esroleo@gmail.com"
+    }
+
+    const gitHub = "esroleo"
+    const engineer = new Engineer(obj, gitHub);
+    expect(engineer.getRole()).toEqual(expect.any(String));
+    //console.log(manager.getRole());
+});
