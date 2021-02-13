fd3958679185387d1a9352b2908dda98771750d5
Esteban Romero <esroleo@gmail.com>
2021-02-13 13:16:02 -0500

    manager class and test completedd

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,32 @@
+const Manager = require('../lib/Manager.js');
+
+
+test('creates a manager object', () => {
+    const obj = {
+        name: "esteban",
+        id: 1234, 
+        email: "esroleo@gmail.com"
+    }
+
+    const officeNumber = "416-123-4567"
+    const manager = new Manager(obj, officeNumber);
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(String));
+});
+
+
+
+  test("gets manager's role", () => {
+    const obj = {
+        name: "esteban",
+        id: 1234, 
+        email: "esroleo@gmail.com"
+    }
+
+    const officeNumber = "416-123-4567"
+    const manager = new Manager(obj, officeNumber);
+    expect(manager.getRole()).toEqual(expect.any(String));
+    //console.log(manager.getRole());
+});
